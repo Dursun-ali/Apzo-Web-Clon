b53983b93f348945e2dadf22cd2199316f55c923
Dursun-ali <99251332+Dursun-ali@users.noreply.github.com>
2023-03-20 17:59:55 +0300

    responsive düzeltmeleri yapıldı

--- js.js
+++ js.js
@@ -1,9 +1,9 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250) {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
     document.getElementsByClassName("mainNavbarContainer-2")[0].style.top = "0";
   } else {
-    document.getElementsByClassName("mainNavbarContainer-2")[0].style.top = "-250px";
+    document.getElementsByClassName("mainNavbarContainer-2")[0].style.top = "-300px";
   }
 }
