9e0d72bd77fe39f4aca664d96b077f61a448be0b
Dursun-ali <99251332+Dursun-ali@users.noreply.github.com>
2023-03-21 13:29:07 +0300

    header düzenlemesi

--- js.js
+++ js.js
@@ -1,9 +1,27 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+  if ( document.documentElement.scrollTop > 300 || screen.width < 991) {
     document.getElementsByClassName("mainNavbarContainer-2")[0].style.top = "0";
   } else {
     document.getElementsByClassName("mainNavbarContainer-2")[0].style.top = "-300px";
   }
+}
+scrollFunction()
+
+function openSidebar(){
+  document.getElementsByClassName('main-scroll-navbar')[0].style.display='block';
+  document.getElementsByClassName('mainNavbarContainer-2')[0].style.width='58.75%'
+  document.getElementsByClassName('mainNavbarContainer-2')[0].style.width='58.75%'
+  document.getElementsByClassName('wrapper-container')[0].style.display='block';
+  
+
+}
+
+function closeSideBar(){
+  
+  document.getElementsByClassName('main-scroll-navbar')[0].style.display='none';
+  document.getElementsByClassName('mainNavbarContainer-2')[0].style.width='100%'
+  document.getElementsByClassName('wrapper-container')[0].style.display='none';
+
 }
