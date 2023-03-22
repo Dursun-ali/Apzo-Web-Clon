95f8946413000a93c10fec79aa99cbcd2cd1b0a3
Dursun-ali <99251332+Dursun-ali@users.noreply.github.com>
2023-03-22 15:26:12 +0300

    son güncellemeler yapıldı

--- js.js
+++ js.js
@@ -1,18 +1,18 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if ( document.documentElement.scrollTop > 300 || screen.width < 991) {
+  if ( document.documentElement.scrollTop > 300||screen.width < 991 ) {
     document.getElementsByClassName("mainNavbarContainer-2")[0].style.top = "0";
   } else {
     document.getElementsByClassName("mainNavbarContainer-2")[0].style.top = "-300px";
   }
+
 }
-scrollFunction()
+scrollFunction();
 
 function openSidebar(){
   document.getElementsByClassName('main-scroll-navbar')[0].style.display='block';
-  document.getElementsByClassName('mainNavbarContainer-2')[0].style.width='58.75%'
-  document.getElementsByClassName('mainNavbarContainer-2')[0].style.width='58.75%'
+  document.getElementsByClassName('mainNavbarContainer-2')[0].style.width='58.75%';
   document.getElementsByClassName('wrapper-container')[0].style.display='block';
   
 
@@ -24,4 +24,15 @@ function closeSideBar(){
   document.getElementsByClassName('mainNavbarContainer-2')[0].style.width='100%'
   document.getElementsByClassName('wrapper-container')[0].style.display='none';
 
+}
+document.getElementById('1').getElementsByClassName('plus-icon')[0].style.display='none';
+
+function findId(id){
+
+for (let i = 1; i <=4; i++) {
+  document.getElementById(i).getElementsByClassName('plus-icon')[0].style.display='none';
+  document.getElementById(i).removeAttribute('disabled','true');
+}
+document.getElementById(id).getElementsByClassName('plus-icon')[0].style.display='block';
+document.getElementById(id).setAttribute('disabled','true');
 }
