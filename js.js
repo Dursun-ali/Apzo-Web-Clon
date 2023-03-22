a441e8c88f0f8f5045ca4e7906eb179c6ae3f4a1
Dursun-ali <99251332+Dursun-ali@users.noreply.github.com>
2023-03-22 23:52:42 +0300

    navbar düzenlemesi

--- js.js
+++ js.js
@@ -14,7 +14,15 @@ function openSidebar(){
   document.getElementsByClassName('main-scroll-navbar')[0].style.display='block';
   document.getElementsByClassName('mainNavbarContainer-2')[0].style.width='58.75%';
   document.getElementsByClassName('wrapper-container')[0].style.display='block';
-  
+}
+function searchOpen(){
+  document.getElementsByClassName('wrapper-container')[0].style.display='block';
+  document.getElementsByClassName('main-search-container')[0].style.display='block';
+
+}
+function searchClose(){
+  document.getElementsByClassName('wrapper-container')[0].style.display='none';
+  document.getElementsByClassName('main-search-container')[0].style.display='none';
 
 }
 
@@ -23,7 +31,6 @@ function closeSideBar(){
   document.getElementsByClassName('main-scroll-navbar')[0].style.display='none';
   document.getElementsByClassName('mainNavbarContainer-2')[0].style.width='100%'
   document.getElementsByClassName('wrapper-container')[0].style.display='none';
-
 }
 document.getElementById('1').getElementsByClassName('plus-icon')[0].style.display='none';
 
@@ -35,4 +42,22 @@ for (let i = 1; i <=4; i++) {
 }
 document.getElementById(id).getElementsByClassName('plus-icon')[0].style.display='block';
 document.getElementById(id).setAttribute('disabled','true');
+}
+
+function isActive(id) {
+  document.getElementById(id).classList.add('navActive');
+  if(!(id == 'nav-item-0'||'nav-item-00')) {
+    document.getElementById('nav-item-0').classList.remove('navActive');
+  }
+}
+
+function isPasive(id) {
+  document.getElementById(id).classList.remove('navActive');
+  document.getElementById(`${id}0`).classList.remove('navActive');
+}
+
+function isDefauldActive() {
+  document.getElementById('nav-item-0').classList.add('navActive');
+  document.getElementById('nav-item-00').classList.add('navActive');
+
 }
